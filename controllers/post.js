37b8682d32f095b63b7bde78113138dd65bf8ce2
ramprit <Rampritsahani@outlook.in>
2018-08-19 09:03:46 +0530

    App

--- controllers/post.js
+++ controllers/post.js
@@ -6,7 +6,19 @@ const bcrypt = require('bcryptjs');
 const secrettoken = require('../middleware/db');
 
 module.exports = {
-  addPost(req, res, next) {
-      
+  addPost(req, res) {
+    const body = {
+      post: req.body.post
+    };
+
+    console.log(body);
+
+    Post.create(body)
+      .then(post => {
+        res.status(HttpStatus.CREATED).json({ message: 'Create sucessfully' });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/postModel');
 const jsonwebtoken = require('jsonwebtoken');
-const authCtrl = require('../controllers/post');
+const postcontroller = require('../controllers/post');
+const verifyToken = require('../helper/AuthHelper');
 
-router.post('/post', authCtrl.addPost);
+router.post('/add-post', postcontroller.addPost);
 
 module.exports = router;
