1298762ef37c8bed2535c4373b767c739e670ba5
ramprit <Rampritsahani@outlook.in>
2018-08-27 19:27:28 +0530

    prod ready

--- controllers/friends.js
+++ controllers/friends.js
@@ -9,8 +9,36 @@ module.exports = {
           _id: req.user._id,
           'following.userFollowed': { $ne: req.body.userFollowed }
         },
-        { $push: {} }
+        {
+          $push: {
+            following: {
+              userFollowed: req.body.userFollowed
+            }
+          }
+        }
+      );
+      await User.update(
+        {
+          _id: req.body.userFollowed,
+          'following.follower': { $ne: req.user._id }
+        },
+        {
+          $push: {
+            followers: {
+              userFollowed: req.body._id
+            }
+          }
+        }
       );
     };
+    followUser()
+      .then(() => {
+        res.status(HttpStatus.OK).json({ message: Msg.OK });
+      })
+      .catch(() => {
+        res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .json({ message: Msg.INTERNALSERVERERROR });
+      });
   }
 };
